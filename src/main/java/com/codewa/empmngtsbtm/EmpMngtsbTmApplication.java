4edc691ef4f6b24e29e726b2a837e72880a43e87
ANOUARLAACHIR <become.engineer88@gmail.com>
2024-10-25 21:29:28 +0100

    first commit

---
+++ src/main/java/com/codewa/empmngtsbtm/EmpMngtsbTmApplication.java
@@ -0,0 +1,13 @@
+package com.codewa.empmngtsbtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMngtsbTmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpMngtsbTmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewa/empmngtsbtm/EmpMngtsbTmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewa.empmngtsbtm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpMngtsbTmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
